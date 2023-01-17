cc5b7c96367dbb659e4bd54f0f958b0410ffbefa
wangchaomin <319151881@qq.com>
2023-01-17 20:59:44 +0800

    feat: 🎸 实现jsx

---
+++ packages/react/src/jsx.ts
@@ -0,0 +1,65 @@
+import { REACT_ELEMENT_TYPE } from 'shared/ReactSymbol';
+import type {
+	Type,
+	Key,
+	Ref,
+	Props,
+	ElementType,
+	ReactElement
+} from 'shared/ReactType';
+const ReactElement = function (
+	type: Type,
+	key: Key,
+	ref: Ref,
+	props: Props
+): ReactElement {
+	const element = {
+		$$typeof: REACT_ELEMENT_TYPE,
+		type,
+		key,
+		ref,
+		props,
+		__mark: 'hhh'
+	};
+	return element;
+};
+
+export const jsx = function (
+	type: ElementType,
+	config: any,
+	...maybeChildren: any[]
+) {
+	let key: Key = null;
+	const props: Props = {};
+	let ref: Ref = null;
+
+	for (const prop of config) {
+		const val = config[prop];
+		if (prop === 'key') {
+			if (val !== undefined) {
+				key = '' + val;
+			}
+			continue;
+		}
+		if (props === 'ref') {
+			if (val !== undefined) {
+				ref = val;
+			}
+			continue;
+		}
+		if (Object.hasOwn(config, prop)) {
+			props[prop] = val;
+		}
+	}
+	const maybeChildrenLength = maybeChildren.length;
+	if (maybeChildrenLength) {
+		if (maybeChildrenLength === 1) {
+			props.children = maybeChildren[0];
+		} else {
+			props.children = maybeChildren;
+		}
+	}
+	return ReactElement(type, key, ref, props);
+};
+
+export const jsxDEV = jsx;
